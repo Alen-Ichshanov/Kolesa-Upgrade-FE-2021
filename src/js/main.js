776ac4761876d270511ba64deb389f9f2f43178e
Alen Ichshanov <alen.ichshanov@gmail.com>
2021-09-26 01:16:56 +0600

    modal window was created using the figma layout

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,20 @@ import '../css/style.css';
 document.querySelector('#app').innerHTML = `
   <h1>Hello, Ален Ищанов!</h1>
 `;
+const modal = document.getElementById('modal-window');
+const btn = document.getElementById('button-modal');
+const span = document.getElementsByClassName('close')[0];
+
+btn.onclick = function () {
+    modal.style.display = 'block';
+};
+
+span.onclick = function () {
+    modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
